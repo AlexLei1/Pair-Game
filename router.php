659e34bbad55629d58cbe11b2d8569202c61bbd6
Alex Lei <sasha.volkov2015@yandex.ru>
2022-12-08 11:45:02 +0300

    changed the file structure

--- router.php
+++ router.php
@@ -6,27 +6,27 @@ $url = explode("/", $_SERVER['REQUEST_URI']);
 // 	echo $url[$i];
 // };
 // переносим код из user в router с помощью require_once
-require_once("bd.php");
-require_once("User.php");
+require_once("php/bd.php");
+require_once("php/User.php");
 
 
 // Проверяем url под индексом [1]
 if ($url[1] == "game") {
-	$content = file_get_contents("../pages/game.html");
+	$content = file_get_contents("pages/game.html");
 } else if ($url[1] == "account") {
-	$content = file_get_contents("../pages/account.html");
+	$content = file_get_contents("pages/account.html");
 } else if ($url[1] == "authorization") {
-	$content = file_get_contents("../pages/auth.html");
+	$content = file_get_contents("pages/auth.html");
 } else if ($url[1] == "register") {
-	$content = file_get_contents("../pages/reg.html");
+	$content = file_get_contents("pages/reg.html");
 } else if ($url[1] == 'addUser') {
 	echo User::addUser($_POST["nickname"], $_POST["email"], $_POST["password"]);
 } else if ($url[1] == 'authUser') {
 	echo User::authUser($_POST["email"], $_POST["password"]);
 } else {
-	$content = file_get_contents("../pages/main.html");
+	$content = file_get_contents("pages/main.html");
 }
 
 // если content не пустой 
-if (!empty($content)) 
-	require_once("template.php");
+if (!empty($content))
+require_once("template.php");
